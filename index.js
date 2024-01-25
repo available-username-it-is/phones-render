327e9502f1d499040169b2e88540732bed237da2
Ivan Roev <roevov2@gmail.com>
2024-01-25 20:44:50 +0200

    frontend deployment

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 var morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('short'))
+app.use(express.static('dist'))
 
 let contacts = [
     { 
@@ -80,7 +83,7 @@ app.post('/api/persons', (request, response) => {
   response.send(newContact)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
